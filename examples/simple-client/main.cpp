ce60935f325d37b5adf2bbe29a8d1c4f6025c327
Christian Paffhausen <thepaffy@thepaffy.de>
2017-05-18 20:32:31 +0200

    Introduced simple-client

--- examples/simple-client/main.cpp
+++ examples/simple-client/main.cpp
@@ -17,14 +17,48 @@
  * along with Netatmo-API-CPP-example.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <unordered_map>
+#include <string>
 #include <iostream>
 #include "nawsapiclient.h"
 
 using namespace netatmoapi;
 using namespace std;
 
-int main(int argc, char **argv) {
+int main() {
+    NAWSApiClient *naWSApiClient = new NAWSApiClient();
+    naWSApiClient->setUsername("username");
+    naWSApiClient->setPassword("password");
+    naWSApiClient->setClientId("clientId");
+    naWSApiClient->setClientSecret("clientSecret");
 
+    unordered_map<string, Station> stationsMap;
+    try {
+        naWSApiClient->login();
+        unordered_map<string, Station> stations = naWSApiClient->requestStationsData("deviceId");
+        for (const pair<string, Station> &station: stations) {
+            cout << "Station id: " << station.first << "\n";
+            unordered_map<string, Module> modules = station.second.modules();
+            for (const pair<string, Module> &module: modules) {
+                cout << "Module name: " << module.second.name() << "\n";
+                cout << "Module id: " << module.second.id() << "\n";
+            }
+        }
+    } catch (const LoginException &loginEx) {
+        cerr << "Catched login exception.\n";
+        cerr << "What: " << loginEx.what() << "\n";
+        cerr << "Credential type: " << loginEx.credentialType() << "\n";
+    } catch (const CurlException &curlEx) {
+        cerr << "Catched curl exception.\n";
+        cerr << "What: " << curlEx.what() << "\n";
+        cerr << "Code: " << curlEx.code() << "\n";
+    } catch (const ResponseException &responseEx) {
+        cerr << "Catched response exception.\n";
+        cerr << "What: " << responseEx.what() << "\n";
+        cerr << "Error: " << responseEx.error() << "\n";
+    }
+
+    delete naWSApiClient;
 
     return 0;
 }
