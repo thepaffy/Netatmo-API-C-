84a8c36cf0620ea068a8a3fcc3992bad68b13cd0
Christian Paffhausen <thepaffy@thepaffy.de>
2017-08-02 21:15:19 +0200

    Updated documentation for Measures

--- src/model/measures.h
+++ src/model/measures.h
@@ -48,33 +48,117 @@ struct Measures {
     Measures();
 
     /**
-     * Converts the pressure trend from string to PressureTrend enum.
-     * @param pressureTrend The pressure trend string.
-     * @return The pressure trend enum value.
+     * Converts the trend from string to Trend enum.
+     * @param trend The trend string.
+     * @return The trend enum value.
      */
     static Trend convertTrendFromString(const std::string &trend);
 
+    /**
+     * @brief Conevert the trend value to string.
+     * @param trend The trend enum value.
+     * @return The trend value as string.
+     */
     static std::string convertTrendToString(Trend trend);
 
+    /**
+     * The measure timestamp.
+     */
     std::uint64_t   mTimeStamp;
+
+    /**
+     * The min temp date.
+     */
     std::uint64_t   mDateMinTemp;
+
+    /**
+     * The max temp date.
+     */
     std::uint64_t   mDateMaxTemp;
+
+    /**
+     * The temperature value.
+     */
     double          mTemperature;
+
+    /**
+     * The CO2 value.
+     */
     double          mCo2;
+
+    /**
+     * The humidity value.
+     */
     double          mHumidity;
+
+    /**
+     * The pressure value.
+     */
     double          mPressure;
+
+    /**
+     * The absolute pressure value.
+     */
     double          mAbsolutePressure;
+
+    /**
+     * The noise value.
+     */
     double          mNoise;
+
+    /**
+     * The rain value.
+     */
     double          mRain;
+
+    /**
+     * The wind strength value.
+     */
     double          mWindStrength;
+
+    /**
+     * The wind angle value.
+     */
     double          mWindAngle;
+
+    /**
+     * The gust strength value.
+     */
     double          mGustStrength;
+
+    /**
+     * The gust angle value.
+     */
     double          mGustAngle;
+
+    /**
+     * The minimum temperature value.
+     */
     double          mMinTemperature;
+
+    /**
+     * The maximum temperature value.
+     */
     double          mMaxTemperature;
+
+    /**
+     * The temperature trend.
+     */
     Trend           mTemperatureTrend;
+
+    /**
+     * The pressure trend.
+     */
     Trend           mPressureTrend;
+
+    /**
+     * The rain sum for the last hour.
+     */
     double          mSumRain1;
+
+    /**
+     * he rain sum for the last 24 hours.
+     */
     double          mSumRain24;
 };
 
